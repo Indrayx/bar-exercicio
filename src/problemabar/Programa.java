edfedbd83545382d63130830692e78609629f6d4
Lucas Augusto <lucasamgs@gmail.com>
2020-05-07 12:06:12 -0300

    Relatorio Gerado

--- src/problemabar/Programa.java
+++ src/problemabar/Programa.java
@@ -14,6 +14,7 @@ public class Programa {
 		char sexo;
 		double consumo,total;
 		
+		// 1 Leitura dos dados
 		System.out.print("Sexo : ");
 		sexo = sc.next().charAt(0);
 		System.out.print("Quantidade de cervejas : ");
@@ -23,6 +24,45 @@ public class Programa {
 		System.out.print("Qauntidade de espetinhos : ");
 		espetinho = sc.nextInt();
 		
+		// 2 Calculos + Relatório gerado
+		
+consumo =  (espetinho * 7) + (cerveja * 5) + (refrigerante * 3);
+		
+		if(sexo == 'F' || sexo == 'f') { 
+			if(consumo > 30 ) { 
+				total = consumo + 8;
+				System.out.println("RELATÓRIO : ");
+				System.out.printf("Consumo = R$%.2f%n " , consumo);
+				System.out.println("Isento de couvert");
+				System.out.println("Ingresso = R$ 8,00");
+				System.out.printf("Valor a pagar = R$%.2f%n " , total);
+			} else {
+				total = consumo + 8 + 4;
+				System.out.println("RELATÓRIO : ");
+				System.out.printf("Consumo = R$%.2f%n " , consumo);
+				System.out.println("Couvert = R$ 4,00");
+				System.out.println("Ingresso = R$ 8,00");
+				System.out.printf("Valor a pagar = R$%.2f%n " , total);
+			}
+		}
+			
+			else {
+				if(consumo > 30 ) { 
+					total = consumo + 10;
+					System.out.println("RELATÓRIO : ");
+					System.out.printf("Consumo = R$%.2f%n " , consumo);
+					System.out.println("Isento de couvert");
+					System.out.println("Ingresso = R$ 10,00");
+					System.out.printf("Valor a pagar = R$%.2f%n " , total);
+				} else {
+					total = consumo + 10 + 4;
+					System.out.println("RELATÓRIO : ");
+					System.out.printf("Consumo = R$%.2f%n " , consumo);
+					System.out.println("Couvert = R$ 4,00");
+					System.out.println("Ingresso = R$ 10,00");
+					System.out.printf("Valor a pagar = R$%.2f%n " , total);
+				}
+			}
 		
 		
 		sc.close();
